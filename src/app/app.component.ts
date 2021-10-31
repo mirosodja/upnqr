083f44ee366825fa58e23cbeba72707cac60de56
Miro Sodja <miro.sodja@gmail.com>
2021-10-31 20:17:06 +0100

    Adding menu components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'upnqr';
+  title = 'Potep.in->Upnqr nalogi';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+// primeng
+import { MenubarModule } from 'primeng/menubar';
+import {TableModule} from 'primeng/table';
+// end primneng
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MenubarModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
